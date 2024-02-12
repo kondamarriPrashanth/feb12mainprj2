a0cf2c56f059ec8820e2019abd3d7ab0bd2b9bf1
abc <abc@gmail.com>
2024-02-12 09:32:43 +0530

    first

---
+++ WebApplication1/Models/Sales.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    [Table("Sales")]
+    public class Sales
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       // public int RAM {  get; set; }
+        //public int Rom {  get; set; }
+        public decimal Price { get; set; }
+    }
+}
